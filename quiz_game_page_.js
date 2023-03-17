294b6e4f2561a39b7d93db49627791edd359e6b8
acetelles <115594718+acetelles@users.noreply.github.com>
2023-03-16 19:32:14 -0600

    Add files via upload

--- quiz_game_page_.js
+++ quiz_game_page_.js
@@ -14,3 +14,30 @@ document.getElementById("number1").value = "";
 document.getElementById("number2").value = "";
 }
 
+question_turn = "player1";
+answer_turn = "player2";
+
+function check() { 
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer)
+    {
+        update_player_1_score = player1_score + 1;
+        document.getElementsById("player1_score").innerHTML =   update_player_1_score;
+    }
+    else
+    {
+        update_player_2_score = player_2_score +  1;
+        document.getElementById("player2_score").innerHTML = update_player_2_score;
+    }
+}
+
+if (question_turn == "player1") 
+{
+    question_turn = "player2"
+    document.getElementById("player_question").innerHTML = "turno para preguntar - " + player2_name;
+}
+else
+{
+    question_turn = "player1"
+    document.getElementById("player_question").innerHTML = "Turno para preguntar - " + player1_name;
+}
